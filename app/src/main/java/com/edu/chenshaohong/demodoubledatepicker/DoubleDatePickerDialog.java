a2c2ac4dd2311ff8e9c1ab0ff37106b366629a00
shshaohong <960020167@qq.com>
2017-02-14 17:34:43 +0800

    初次提交

---
+++ app/src/main/java/com/edu/chenshaohong/demodoubledatepicker/DoubleDatePickerDialog.java
@@ -0,0 +1,174 @@
+package com.edu.chenshaohong.demodoubledatepicker;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.DatePicker;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by shaohong on 2017-2-14.
+ */
+
+public class DoubleDatePickerDialog extends AlertDialog implements DialogInterface.OnClickListener, DatePicker.OnDateChangedListener {
+
+    private static final String START_YEAR = "start_year";
+    private static final String END_YEAR = "end_year";
+    private static final String START_MONTH = "start_month";
+    private static final String END_MONTH = "end_month";
+    private static final String START_DAY = "start_day";
+    private static final String END_DAY = "end_day";
+
+    private final DatePicker mDatePicker_start;
+    private final DatePicker mDatePicker_end;
+    private final OnDateSetListener mCallBack;
+
+    public DoubleDatePickerDialog(Context context, OnDateSetListener callBack, int year, int monthOfYear, int dayOfMonth) {
+        this(context, 0, callBack, year, monthOfYear, dayOfMonth);
+    }
+
+
+    public DoubleDatePickerDialog(Context context, int theme, OnDateSetListener callBack, int year, int monthOfYear,
+                                  int dayOfMonth) {
+        this(context, 0, callBack, year, monthOfYear, dayOfMonth, true);
+    }
+
+    public DoubleDatePickerDialog(Context context, int theme, OnDateSetListener callBack, int year, int monthOfYear,
+                                  int dayOfMonth, boolean isDayVisible) {
+        super(context, theme);
+
+        mCallBack = callBack;
+
+        Context themeContext = getContext();
+        setButton(BUTTON_POSITIVE, "确 定", this);
+        setButton(BUTTON_NEGATIVE, "取 消", this);
+        setIcon(0);
+
+        LayoutInflater inflater = (LayoutInflater) themeContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.date_picker_dialog, null);
+        setView(view);
+        mDatePicker_start = (DatePicker) view.findViewById(R.id.datePickerStart);
+        mDatePicker_end = (DatePicker) view.findViewById(R.id.datePickerEnd);
+        mDatePicker_start.init(year, monthOfYear, dayOfMonth, this);
+        mDatePicker_end.init(year, monthOfYear, dayOfMonth, this);
+
+        // 如果要隐藏当前日期，则使用下面方法。
+        if (!isDayVisible) {
+            hidDay(mDatePicker_start);
+            hidDay(mDatePicker_end);
+        }
+    }
+
+    private void hidDay(DatePicker mDatePicker) {
+        Field[] datePickerfFields = mDatePicker.getClass().getDeclaredFields();
+        for (Field datePickerField : datePickerfFields) {
+            if ("mDaySpinner".equals(datePickerField.getName())) {
+                datePickerField.setAccessible(true);
+                Object dayPicker = new Object();
+                try {
+                    dayPicker = datePickerField.get(mDatePicker);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (IllegalArgumentException e) {
+                    e.printStackTrace();
+                }
+                ((View) dayPicker).setVisibility(View.GONE);
+            }
+        }
+    }
+
+    public void onClick(DialogInterface dialog, int which) {
+        // Log.d(this.getClass().getSimpleName(), String.format("which:%d",
+        // which));
+        // 如果是“取 消”按钮，则返回，如果是“确 定”按钮，则往下执行
+        if (which == BUTTON_POSITIVE)
+            tryNotifyDateSet();
+
+    }
+
+    @Override
+    public void onDateChanged(DatePicker view, int year, int month, int day) {
+        if (view.getId() == R.id.datePickerStart)
+            mDatePicker_start.init(year, month, day, this);
+        if (view.getId() == R.id.datePickerEnd)
+            mDatePicker_end.init(year, month, day, this);
+        // updateTitle(year, month, day);
+    }
+
+    public DatePicker getDatePickerStart() {
+        return mDatePicker_start;
+    }
+
+    public DatePicker getDatePickerEnd() {
+        return mDatePicker_end;
+    }
+
+    public void updateStartDate(int year, int monthOfYear, int dayOfMonth) {
+        mDatePicker_start.updateDate(year, monthOfYear, dayOfMonth);
+    }
+
+    public void updateEndDate(int year, int monthOfYear, int dayOfMonth) {
+        mDatePicker_end.updateDate(year, monthOfYear, dayOfMonth);
+    }
+
+    private void tryNotifyDateSet() {
+//        if (mDatePicker_end.getDayOfMonth() - mDatePicker_start.getDayOfMonth() < 0) {
+//            if (mDatePicker_end.getMonth() - mDatePicker_start.getMonth() < 0) {
+//                if (mDatePicker_end.getYear() - mDatePicker_start.getYear() < 0) {
+//                    Toast.makeText(getContext(), "结束时间不能早于开始时间", Toast.LENGTH_SHORT).show();
+//                    return;
+//                }
+//            }
+//        }
+        if (mCallBack != null) {
+            mDatePicker_start.clearFocus();
+            mDatePicker_end.clearFocus();
+            mCallBack.onDateSet(mDatePicker_start, mDatePicker_start.getYear(), mDatePicker_start.getMonth(),
+                    mDatePicker_start.getDayOfMonth(), mDatePicker_end, mDatePicker_end.getYear(),
+                    mDatePicker_end.getMonth(), mDatePicker_end.getDayOfMonth());
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        // tryNotifyDateSet();
+        super.onStop();
+    }
+
+    @Override
+    public Bundle onSaveInstanceState() {
+        Bundle state = super.onSaveInstanceState();
+        state.putInt(START_YEAR, mDatePicker_start.getYear());
+        state.putInt(START_MONTH, mDatePicker_start.getMonth());
+        state.putInt(START_DAY, mDatePicker_start.getDayOfMonth());
+        state.putInt(END_YEAR, mDatePicker_end.getYear());
+        state.putInt(END_MONTH, mDatePicker_end.getMonth());
+        state.putInt(END_DAY, mDatePicker_end.getDayOfMonth());
+        return state;
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        int start_year = savedInstanceState.getInt(START_YEAR);
+        int start_month = savedInstanceState.getInt(START_MONTH);
+        int start_day = savedInstanceState.getInt(START_DAY);
+        mDatePicker_start.init(start_year, start_month, start_day, this);
+
+        int end_year = savedInstanceState.getInt(END_YEAR);
+        int end_month = savedInstanceState.getInt(END_MONTH);
+        int end_day = savedInstanceState.getInt(END_DAY);
+        mDatePicker_end.init(end_year, end_month, end_day, this);
+    }
+
+    public interface OnDateSetListener {
+
+
+        void onDateSet(DatePicker startDatePicker, int startYear, int startMonthOfYear, int startDayOfMonth,
+                       DatePicker endDatePicker, int endYear, int endMonthOfYear, int endDayOfMonth);
+    }
+}
